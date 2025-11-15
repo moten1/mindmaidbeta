7c86f9fffe5f2144aeadbae9b7bf7cae9de19a32
Kashif Moten <energyclinicglobal@gmail.com>
2025-11-15 13:17:38 +0500

    Clean structure: all backend files in backend folder

--- routes/admin.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-router.get('/stats', (req,res)=> res.json({ users: 10, uploads: 24 }));
-router.get('/export/users', (req,res)=> res.send('id,email\nuser1,user1@example.com'));
-module.exports = router;
